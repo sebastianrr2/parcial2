e4053e63eed56a8b535175ff06094d035bb379bd
sebastian ramirez <sebastramireza@gmail.com>
2025-05-17 18:55:24 -0500

    evaluacion module

--- src/evalucacion/evalucacion.module.ts
+++ src/evalucacion/evalucacion.module.ts
@@ -1,18 +1,21 @@
 /* eslint-disable prettier/prettier */
+// evalucacion.module.ts
 import { Module } from '@nestjs/common';
-
+import { EvalucacionService } from './evalucacion.service';
+import { EvalucacionController } from './evalucacion.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Evalucacion } from './entities/evalucacion.entity';
-import { Profesor } from '../profesor/entities/profesor.entity';
 import { Proyecto } from '../proyecto/entities/proyecto.entity';
-import { EvalucacionService } from './evalucacion.service';
+import { ProfesorModule } from '../profesor/profesor.module'; // importa el módulo correcto
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Evalucacion, Profesor, Proyecto]), // <-- esto es CLAVE
+    TypeOrmModule.forFeature([Evalucacion, Proyecto]),
+    ProfesorModule, // <== esto permite que EvalucacionService use ProfesorService
   ],
+  controllers: [EvalucacionController],
   providers: [EvalucacionService],
-  exports: [EvalucacionService],
 })
 export class EvalucacionModule {}
+
 
